f445d07bf3c99caa562614b6c0adbcd33e390f9e
github username <josephspinelli@yahoo.com>
2019-08-11 01:13:18 -0500

    program successfully counts number of words in text file

--- src/WordCount.java
+++ src/WordCount.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 import java.io.File;
 
 public class WordCount {
-    public static void main(String [] args) {
+    // throws Exception in case there is a problem reading file or if it isn't there
+    public static void main(String [] args) throws Exception {
+        File file = new File("GrimmStories.txt");
+        Scanner scanner = new Scanner(file);
 
+        int totalWordCount = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            int wordLength = line.split(" ").length;
+            totalWordCount += wordLength;
+        }
+
+        System.out.println(totalWordCount);
     }
 }
